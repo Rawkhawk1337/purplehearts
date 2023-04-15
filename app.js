d9e3dff309441ecdf43bac237ecbbcb32f4d4487
tvizvary <tvizvary17@gmail.com>
2023-04-14 22:45:31 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const caption = document.getElementById('caption');
   
     const slideshowLinks = [
-      'img/1.png',
-      'img/2.png',
+      'purplehearts/img/1.png',
+      'purplehearts/img/2.png',
     ];
     const captions = [
         `I knew from the moment I laid eyes on you that you were something special. You were jaw-dropping. Not only because of your appearance, but everything about you took my breath away. No one else understood me. I kept it secret for a long time because I thought I would be rejected and that you were too good for me. I knew that Braiden wasn’t good for you, and I wanted you to have a better life. `,
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
   
     let slideshowCounter = 0;
   
-    currentImg.setAttribute('src', 'img/1.png');
+    currentImg.setAttribute('src', 'purplehearts/img/1.png');
     caption.innerText = (captions[slideshowCounter])
   
     slideshowContainer.addEventListener('click', () => {
