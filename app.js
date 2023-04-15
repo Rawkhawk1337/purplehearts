67e51b741bdf23182e6edd64e9ee280c55e44fac
tvizvary <tvizvary17@gmail.com>
2023-04-14 23:42:40 -0700

    work?

--- app.js
+++ app.js
@@ -4,17 +4,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const caption = document.getElementById('caption');
   
     const slideshowLinks = [
-      'purplehearts/img/1.png',
-      'purplehearts/img/2.png',
+      './img/1.png',
+      './img/2.png',
+      './img/3.png',
+      './img/4.png',
+      './img/5.png',
     ];
     const captions = [
         `I knew from the moment I laid eyes on you that you were something special. You were jaw-dropping. Not only because of your appearance, but everything about you took my breath away. No one else understood me. I kept it secret for a long time because I thought I would be rejected and that you were too good for me. I knew that Braiden wasn’t good for you, and I wanted you to have a better life. `,
         `When we moved in “together,” I was worried because you were so much nicer to me than most people were. You had a truly kind heart, and I could tell. You helped me put my bed together when no one else would. It’s funny thinking back that I hugged Alex when I was sad, because he didn’t do anything to help me feel at home. You did everything for me.`,
+        `When you came to take care of me that day, that was when my life really changed. No one had ever cared for me like that before. It touched me in a way I didn’t fully understand until later. You were my angel, and I didn’t want to let go. I wanted to be with you for as long as I could. This moment led to everything else. Thank you for being so nice and changing my life for the better.`,
+        `It was the culmination of everything. I couldn’t handle the reality of having to trade all of my friends for my one true love, so I reacted explosively. You were there to comfort me, as you always are. We knew the situation was terrible, but we were in love. We drove off to be together, alone, and that night I truly realized how much I loved you. I can still remember how the music from my car sounded that night, and how much I wanted to hold your hand and hug you.`,
+        `Others look at this night and say it was my greatest mistake. That I’ve caused so much pain by what I did. But I believe this was the best decision I ever made. I definitely wanted to kiss, but I went down to you not knowing whether or not I would go through with it. I just wanted to be near you and have fun with you. I loved you so much and seeing you alone and in pain hurt me so much. It still hurts me so much. This night it forever is my memory, and nothing will replace it.`,
+
     ]
   
     let slideshowCounter = 0;
   
-    currentImg.setAttribute('src', 'purplehearts/img/1.png');
+    currentImg.setAttribute('src', './img/1.png');
     caption.innerText = (captions[slideshowCounter])
   
     slideshowContainer.addEventListener('click', () => {
@@ -28,3 +35,5 @@ document.addEventListener('DOMContentLoaded', () => {
       caption.innerText = (captions[slideshowCounter])
     });
   });
+
+  
